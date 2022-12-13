49642c416b282e77acb73a9a99d07de5584ef907
Denys Morozov <ajax3101@gmail.com>
2022-12-13 19:59:54 +0200

    all workaet!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import os
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
@@ -19,10 +20,25 @@ def create_and_upload_file(file_name='test.txt', file_content='Hi!'):
         return 'Trouble, check your code please!'
 
 
-def main():
-    print(create_and_upload_file(
-        file_name='hello.txt', file_content='Hello Freands!'))
+def upload_dir(dir_path=''):
+    try:
+        drive = GoogleDrive(gauth)
 
+        for file_name in os.listdir(dir_path):
+
+            my_file = drive.CreateFile({'title': f'{file_name}'})
+            my_file.SetContentFile(os.path.join(dir_path, file_name))
+            my_file.Upload()
+
+            print(f'File {file_name} was uploaded!')
+        return 'Success! All files uploaded! Super!'
+    except Exception as _ex:
+        return 'Trouble, check your code please!'
+
+
+def main():
+    #print(create_and_upload_file(file_name='hello.txt', file_content='Hello Freands!'))
+    print(upload_dir(dir_path='C:/work/g-drive/x-files'))
 
 if __name__ == '__main__':
     main()
